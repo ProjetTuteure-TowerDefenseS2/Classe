2e578f066dc74975f06a982f12b30f5805e1ed4b
Bcricholf <flobarflo@gmail.com>
2015-02-12 16:41:32 +0100

    Update Tir.h

--- Tir.h
+++ Tir.h
@@ -17,13 +17,12 @@ const int MIN_LARGEUR = 0;
 class Tir
 {
     public:
-        //Tir(string nomTexture) throw (string);
         Tir(string nomTexture);
         virtual ~Tir();
-        void creationTir(Texture texture_Tour, const Vector2f posTour, const Vector2f posEnnemi, int vitesse) throw(string);
+        void creationTir(Texture texture_Tour, const Vector2f posTour, const Vector2f posEnnemi, int vitesse = 1, int zone = 1) throw(string);
         void deplacementTir();
         void afficheTir(RenderWindow window);
-        bool collisionEnnemi(Texture ennemi, const Vector2f posEnnemi) const;
+        bool collisionEnnemi(Texture ennemi, const Vector2f posEnnemi);
         void verifDepassementEcran();
         Sprite getSprite() const {return m_tir;}
         bool getTirer() const {return m_tirer;}
@@ -34,12 +33,13 @@ class Tir
         Texture m_texture;
         Sprite m_tir;
         float m_vitesse;
+        float m_tempsZone;
         float m_valeurDeplacement_X;
         float m_valeurDeplacement_Y;
         float m_valeurRotation;
         bool m_tirer;
         Clock m_clock;
-        Time m_temps;
+        Clock m_clockZone;
 };
 
 #endif // TIR_H
